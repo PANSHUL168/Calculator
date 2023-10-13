98faaac35f9a3a02d9b0b3c637b44fc1326822ca
PANSHUL SINGH <panshul.magic@gmail.com>
2023-10-13 21:26:36 +0530

    first commit

---
+++ lib/colours.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+const operatorColor = Color(0xff272727);
+const buttonColor = Color(0xff191919);
+const orangeColor = Color.fromARGB(255, 3, 244, 248);
---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+
+// COLOURS USED IN UI
+    const operatorColor = Color(0xff272727);
+    const buttonColor = Color(0xff191919);
+    const greenColor = Color.fromARGB(255, 1, 254, 26);
+
+void main(){
+  runApp(const MaterialApp(
+    home:Calculator()
+  ));
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+
+
+        // OUTPUT AREA
+        children: [
+          Expanded(
+            child:Container(
+              width: double.infinity,
+              padding:const EdgeInsets.all(12),
+              child:const Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Output',
+                    style:TextStyle(
+                      color:Colors.white,
+                      fontSize: 50
+                    ),
+                  )
+                ],
+              )
+            )
+          ),
+
+
+          //SPACING IN BETWEEN
+          const SizedBox(
+            height:40
+          ),
+
+
+
+          //BUTTON AREA
+          // button() widget used is defined below
+          Row(
+            children: [
+              button(text:'AC',tColor:greenColor,buttonBgColor: operatorColor),
+              button(text:'<',tColor:greenColor,buttonBgColor: operatorColor),
+              button(text:'()',tColor:greenColor,buttonBgColor: operatorColor),
+              button(text:'/',tColor:greenColor,buttonBgColor: operatorColor)
+            ],
+          ),
+          Row(
+            children: [
+              button(text:'7'),
+              button(text:'8'),
+              button(text:'9'),
+              button(text:'*',tColor:greenColor,buttonBgColor: operatorColor)
+            ],
+          ),
+          Row(
+            children: [
+              button(text:'4'),
+              button(text:'5'),
+              button(text:'6'),
+              button(text:'-',tColor:greenColor,buttonBgColor: operatorColor)
+            ],
+          ),
+          Row(
+            children: [
+              button(text:'1'),
+              button(text:'2'),
+              button(text:'3'),
+              button(text:'+',tColor:greenColor,buttonBgColor: operatorColor)
+            ],
+          ),
+          Row(
+            children: [
+              button(text:'%',tColor:greenColor,buttonBgColor: operatorColor),
+              button(text:'0'),
+              button(text:'.'),
+              button(text:'=',buttonBgColor:greenColor)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  //BUTTON LAYOUT DEFINED IN A FUNNCTION 
+  Widget button(
+    {text , tColor = Colors.white , buttonBgColor = buttonColor}
+  ){
+    return Expanded(
+      child:Container(
+        margin:const EdgeInsets.all(8),
+        child:ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12)
+            ),
+            padding:const EdgeInsets.all(22),
+            backgroundColor: buttonBgColor,
+          ),
+          child:Text(
+            text,
+            style:TextStyle(
+              color:tColor,
+              fontSize:25,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+
+
+          // ON PRESSED FUNCTION
+          onPressed:(){},
+          //...............
+
+
+        )
+      )
+    );
+  }
+}
+
